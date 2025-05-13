e64093c63db3bacded10de70601fcb98e4a0d6a1
sarrisv <v.sarris@pitt.edu>
2025-05-13 04:07:20 +0000

    Deploying to gh-pages from @ sarrisv/sarrisv.github.io@67f55c34fec1a322defe3fa451dc83c2a471b6b7 🚀

---
+++ assets/js/cronitor-analytics-setup.js
@@ -0,0 +1,6 @@
+window.cronitor =
+  window.cronitor ||
+  function () {
+    (window.cronitor.q = window.cronitor.q || []).push(arguments);
+  };
+cronitor("config", { clientKey: "7ca366287183c644a3fd1f64b8da412f" });
